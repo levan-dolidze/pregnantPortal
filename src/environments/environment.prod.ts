94d15659d77e7e5ee805da479facb58dc1a2ef36
levan-dolidze <64006772+levan-dolidze@users.noreply.github.com>
2023-11-01 12:44:27 +0400

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,15 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyA3zBUW_7T0rtETyMYEhW7JmBiRsyMy9Gk",
+    authDomain: "pregnantportal.firebaseapp.com",
+    projectId: "pregnantportal",
+    storageBucket: "pregnantportal.appspot.com",
+    messagingSenderId: "348776876701",
+    appId: "1:348776876701:web:06baa2f97c7a6df8392274",
+    measurementId: "G-L534XRVX0X"
+  },
+  apiUrl: 'https://pregnantportal-default-rtdb.firebaseio.com',
+  uid: 'lVmSdLZMNRYjEpGnTQFpbuIeBEE2'
+
 };
