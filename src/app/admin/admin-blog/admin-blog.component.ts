2c55eee34fb6adecf97c5ac23a57510e1e2ce99e
levan-dolidze <64006772+levan-dolidze@users.noreply.github.com>
2023-11-26 18:56:36 +0400

    up

--- src/app/admin/admin-blog/admin-blog.component.ts
+++ src/app/admin/admin-blog/admin-blog.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { AngularEditorConfig } from '@kolkov/angular-editor';
 import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { finalize } from 'rxjs';
 
 @Component({
   selector: 'app-admin-blog',
@@ -16,15 +18,22 @@ export class AdminBlogComponent implements OnInit {
   }
 
   fb = inject(FormBuilder)
+   storage = inject(AngularFireStorage) 
+
 
 
   initForm() {
     this.form = this.fb.group({
-      htmlContent: new FormControl(null)
+      htmlContent: new FormControl(null),
+      img:new FormControl(null)
     })
   }
 
-  form: FormGroup
+  form: FormGroup;
+
+  get f() {
+    return this.form.controls
+  }
 
   editorConfig: AngularEditorConfig = {
     editable: true,
@@ -74,7 +83,60 @@ export class AdminBlogComponent implements OnInit {
   };
 
 
-  get f() {
-    return this.form.controls
+  imgURL: unknown;
+  selectedImage: unknown;
+
+  selectedStuff: unknown;
+
+
+  showPreview(event: any) {
+    if (event.target.files && event.target.files[0]) {
+      const reader = new FileReader();
+      reader.onload = (e: any) => this.imgURL = e.target.result;
+
+      reader.readAsDataURL(event.target.files[0])
+      this.selectedStuff = event.target.files[0];
+
+      this.addFile(this.selectedStuff)
+
+    } else {
+      this.imgURL = 'https://t4.ftcdn.net/jpg/03/46/93/61/360_F_346936114_RaxE6OQogebgAWTalE1myseY1Hbb5qPM.jpg';
+      this.selectedStuff = null;
+    }
+  };
+
+  isLoading:boolean=false
+
+  reqStuffImg: string
+  reqCourseImg: string
+  addFile(selectedFile: any) {
+
+    this.isLoading=true
+    var filePath = `${selectedFile.name}_${new Date().getTime()}`
+    const fileRef = this.storage.ref(filePath)
+    this.storage.upload(filePath, selectedFile).snapshotChanges().pipe(
+      finalize(() => {
+        //url ში გვაქვს ახალი ატვირთული სურათი
+        let service = localStorage.getItem('service');
+        fileRef.getDownloadURL().subscribe((url: any) => {
+          if (url) {
+            this.reqStuffImg = url
+            this.reqCourseImg=url
+
+            this.isLoading=false
+            // this.httpAdmin.insertMenu(obj)
+
+
+          }
+        })
+      })
+    ).subscribe(() => { })
+  };
+
+
+
+
+  onAddBlog(){
+    console.log(this.form.value)
   }
 }
